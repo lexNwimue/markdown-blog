216bca77430c9ed0512d1c28e9ddaebf950bfc42
lexNwimue <nwimueprogress@gmail.com>
2022-05-19 22:37:07 +0100

    Designed index.html skeleton under /pages and added css/styles.css for styling the index page.Yet to design Create Blog page

--- blogRoutes.js
+++ blogRoutes.js
@@ -2,12 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log("List of blogs go here...");
-});
-
 router.get("/:id", (req, res) => {
-  console.log("Blog with particular ID goes here");
+  //   console.log("Blog with particular ID goes here");
 });
 
 export default router;
